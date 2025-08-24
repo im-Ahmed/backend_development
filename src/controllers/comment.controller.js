93ea64d5cd68b71caec58b8a7fd1eddff7d9714b
im-Ahmed <ahmedfarooqim3@gmail.com>
2025-08-24 16:12:19 +0500

    Implement CRUD operations for comments in the comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,88 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-
-})
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+  const { videoId } = req.params;
+  const { content } = req.body;
+  if (!videoId || videoId.length != 24) {
+    throw new ApiError(400, "Video Id is missing or Invalid Id");
+  }
+  if (!content) {
+    throw new ApiError(400, "Comment content is required");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+  if (!comment) {
+    throw new ApiError(500, "Something went wrong while adding comment");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment is addded successfully"));
+  // TODO: add a comment to a video
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  const { commentId } = req.params;
+  const { newContent } = req.body;
+  if (!commentId || commentId.length != 24) {
+    throw new ApiError(400, "comment Id is missing or Invalid Id");
+  }
+  if (!newContent) {
+    throw new ApiError(400, "Comment content is required for updating");
+  }
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      content: newContent,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!updatedComment) {
+    throw new ApiError(500, "Something went wrong while updating comment");
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedComment, "Comment is updated successfully")
+    );
+
+  // TODO: update a comment
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+  const { commentId } = req.params;
+  if (!commentId || commentId.length != 24) {
+    throw new ApiError(400, "comment Id is missing or Invalid Id");
+  }
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+  try {
+    await Comment.findByIdAndDelete(commentId);
+  } catch (err) {
+    throw new ApiError(
+      500,
+      err.message || "Something went wrong while deleting comment"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Comment is deleted successfully"));
+  // TODO: delete a comment
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
