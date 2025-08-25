8476a1cc1e583a53568fc4c74547a957c1953c49
im-Ahmed <ahmedfarooqim3@gmail.com>
2025-08-25 13:35:41 +0500

    Implement video comments retrieval with validation and user lookup

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,6 +8,49 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+  if (!videoId || videoId.length != 24) {
+    throw new ApiError(400, "Video Id is missing or Invalid Id");
+  }
+
+  const aggregate = Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "commentedBy",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        commentedBy: 1,
+      },
+    },
+  ]);
+  const options = {
+    limit,
+    page,
+  };
+  const comments = await Comment.aggregatePaginate(aggregate, options);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, comments.docs, "Comments are fetched successfully")
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
